37412afb268ecd932188187427c4e1c2ddb141ff
suganyaselvarajan <suganya@poshvine.com>
2020-10-30 16:13:32 +0530

    Remove unwanted code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,20 +13,4 @@ class RecipesController < ApplicationController
   def load_recipe
     @recipe = Recipe.find(params[:id])
   end
-
-  def get_tags
-    tags = @recipe.fields[:tags]&.map do |tag|
-      tag = get_entry(tag.id)
-      tag.fields[:name]
-    end
-    tags ||= []
-  end
-
-  def get_chef
-    cid = @recipe.fields[:chef]&.id
-    return 'N/A' unless cid
-    chef = get_entry(cid)
-    chef.fields[:name]
-  end
-
 end
--- config/initializers/contentful.rb
+++ config/initializers/contentful.rb
@@ -1,12 +1,6 @@
-ContentfulRails.configure do |config|
-  config.authenticate_webhooks = false # false here would allow the webhooks to process without basic auth
-  # config.webhooks_username = "a basic auth username"
-  # config.webhooks_password = "a basic auth password"
-  config.access_token = "7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c"
-  # config.preview_access_token = "your preview access token"
-  config.management_token = "7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c"
-  config.space = "kk2bw5ojx476"
+ContentfulModel.configure do |config|
+  config.access_token = ENV.fetch('ACCESS_TOKEN')
+  config.management_token = ENV.fetch('MANAGEMENT_TOKEN')
+  config.space = ENV.fetch('SPACE')
   config.environment = "master"
-  config.contentful_options = {
-  }
 end
